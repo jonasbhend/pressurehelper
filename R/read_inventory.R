4e8b2c313e78596d937223d584be9c71be0bb792
Jonas Bhend <jonas.bhend@gmx.ch>
2014-08-11 16:07:57 +0200

    read the pressure inventory and metadata

---
+++ R/read_inventory.R
@@ -0,0 +1,33 @@
+#' read_inventory
+#' 
+#' Reads metadata in common repository for later use
+#' 
+#' @param file path to file containing inventory information
+#' 
+#' @keywords util
+#' @export
+read_inventory <- function(file='~/Unibe/pressure/Working_inventory_pressure_1815-17.xls'){
+  ## load the input file
+  wb <- loadWorkbook(file)
+  ## set empty cells to missing
+  setMissingValue(wb, value="")
+  
+  ## read the header
+  inventory <- readWorksheet(wb, sheet=1)
+  
+  ## change the names for ease of access
+  names(inventory)[grep('Location.Flag', names(inventory))] <- 'Location.Flag'
+  names(inventory)[grep('Elevation.Flag', names(inventory))] <- 'Elevation.Flag'
+  names(inventory)[names(inventory) == 'Elevation..m.'] <- 'Elevation'
+  names(inventory)[grep('Times.of.observation', names(inventory))] <- 'Times.of.observation'
+  names(inventory)[grep('Period.available', names(inventory))] <- 'Period.available'
+  names(inventory)[names(inventory) == 'Temperature.at.the.barometer'] <- 'hasTP'
+  names(inventory)[names(inventory) == 'Reduced.for.temperature'] <- 'Tcorr'  
+  names(inventory)[names(inventory) == 'Reduced.to.sea.level'] <- 'hasQFF'  
+  
+  ## change some of the variables to boolean
+  inventory$hasQFF <- inventory$hasQFF == 'X' & !is.na(inventory$hasQFF)
+  inventory$hasTP <- inventory$hasTP == 'X' & !is.na(inventory$hasTP)
+  
+  return(inventory)
+}
