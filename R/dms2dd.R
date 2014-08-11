f81322e6c7e0ddb2244d44bd8ba34ef4a239b1e4
Jonas Bhend <jonas.bhend@gmx.ch>
2014-08-11 16:07:33 +0200

    Added functionality to also read NSWE and numeric values for completeness

--- R/dms2dd.R
+++ R/dms2dd.R
@@ -13,20 +13,32 @@
 #' 
 #' @export
 dms2dd <- function(x){
-  if (is.numeric(x)) stop('Cannot convert numeric values without units')
   
-  ## extract degrees, minutes and seconds
-  deg <- as.numeric(sapply(strsplit(x, '°'), function(x) x[1]))
-  min <- as.numeric(sapply(strsplit(gsub(".*°", "", x), "'"), function(x) x[1]))
-  sec <- as.numeric(gsub("\"", "", gsub(".*'", "", x)))
-  degsign <- c(1, -1)[(substring(x, 1, 1) == '-')*1 + 1]
-  
-  ## set seconds and minutes to zero if missing
-  min[is.na(min)] <- 0
-  sec[is.na(sec)] <- 0
-  
-  ## compute decimal degrees
-  dec <- deg + degsign*min/60 + degsign*sec/3600
+  if (all(is.numeric(x))){
+    dec <- x
+  } else {
+    
+    ## extract degrees, minutes and seconds
+    ## suppress the warnings
+    oldwarn <- options('warn')
+    options(warn=-1)
+    deg <- as.numeric(sapply(strsplit(x, '°'), function(x) x[1]))
+    min <- as.numeric(sapply(strsplit(gsub(".*°", "", x), "'"), function(x) x[1]))
+    sec <- as.numeric(gsub("\".*", "", gsub(".*'", "", x)))
+    options(warn=oldwarn$warn)
+    
+    ## get the sign of the longitude or latitudes
+    degsign <- rep(1, length(x))
+    deg.i <- c(grep('W', x), grep('S', x), which(substr(x, 1, 1) == '-'))
+    degsign[deg.i] <- -1
+    
+    ## set seconds and minutes to zero if missing
+    min[is.na(min)] <- 0
+    sec[is.na(sec)] <- 0
+    
+    ## compute decimal degrees
+    dec <- deg + degsign*min/60 + degsign*sec/3600    
+  }
   
   return(dec)
 }
