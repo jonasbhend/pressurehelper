6e397cace29b598a3a483e07da5414a9beda932c
Jonas Bhend <jonas.bhend@gmx.ch>
2014-08-12 14:35:34 +0200

    Added verbosity switch

--- R/temperature2SI.R
+++ R/temperature2SI.R
@@ -5,6 +5,7 @@
 #' 
 #' @param x temperature values
 #' @param units temperature units
+#' @param verbose logicl, should info about conversion factors be displayed
 #' 
 #' @references
 #' http://www.thefullwiki.org/Conversion_of_units
@@ -17,29 +18,29 @@
 #' 
 #' @keywords util
 #' @export
-temperature2SI <- function(x, units='C'){
+temperature2SI <- function(x, units='C', verbose=TRUE){
   # simplify units
   units <- gsub('\\.', '', gsub('degrees ', '', gsub('degree ', '', gsub('deg. ', '', gsub('°', '', units)))))
   if (units %in% c('C', 'Celsius')){
-    print('Convert Celsius to Kelvin')
+    if (verbose) print('Convert Celsius to Kelvin')
     xout <- x + 273.15
   } else if (units %in% c('F', 'Fahrenheit')){
-    print('Convert Fahrenheit to Kelvin')
+    if (verbose) print('Convert Fahrenheit to Kelvin')
     xout <- (x + 459.67) * 5 / 9
   } else if (units %in% c('R', 'Re', 'Ré', 'Reaumur', 'Réaumur')){
-    print('Convert Réaumur to Kelvin')
+    if (verbose) print('Convert Réaumur to Kelvin')
     xout <- x * 5 / 4 + 273.15
   } else if (units %in% c('D', 'De', 'Delisle')){
-    print('Convert Delisle to Kelvin')
+    if (verbose) print('Convert Delisle to Kelvin')
     xout <- 373.15 - x * 2/3
   } else if (units %in% c('N', 'Newton')){
-    print('Convert Newton to Kelvin')
+    if (verbose) print('Convert Newton to Kelvin')
     xout <- x * 100 / 33 + 273.15
   } else if (units %in% c('Ro', 'Rø', 'Roemer', 'Rømer')){
-    print('Convert Rømer to Kelvin')
+    if (verbose) print('Convert Rømer to Kelvin')
     xout <- (x - 7.5) * 40/21 + 273.15
   } else if (units %in% c('Ra', 'Rankine')){
-    print('Convert Rankine to Kelvin')
+    if (verbose) print('Convert Rankine to Kelvin')
     xout <- x * 5 / 9
   }
     
