5c041bfe5644fc8da70515e27a7753447d55d9a2
Jonas Bhend <jonas.bhend@gmx.ch>
2014-08-12 09:00:08 +0200

    Added verbose argument to suppress output

--- R/length2SI.R
+++ R/length2SI.R
@@ -9,6 +9,7 @@
 #' @param units length units to be used (see details)
 #' @param base conversion factors for columns of matrix if non-standard
 #' units are used, character if in base list
+#' @param verbose logical, should information on conversion factors be displayed?
 #' 
 #' @references
 #' http://www.thefullwiki.org/Conversion_of_units
@@ -19,7 +20,7 @@
 #' 
 #' @keywords util
 #' @export
-length2SI <- function(x, units='imperial', base=NULL){
+length2SI <- function(x, units='imperial', base=NULL, verbose=TRUE){
   
   baselist <- c(mile=1609.344,
                 yard=0.9144,
@@ -28,12 +29,12 @@ length2SI <- function(x, units='imperial', base=NULL){
                 line=0.9144 / 36 / 12,
                 ell=0.9144 * 1.25, 
                 finger=0.022225,
-                tum=0.02474)
+                tum=0.02969)
   
   if (is.matrix(x) | is.data.frame(x)){
     if (is.null(base) & units == 'imperial') base <- c('yard', 'foot', 'inch')
     if (is.null(base)) stop('Base for conversion not supplied')
-    print(paste('Convert with base', paste(base, collapse=', ')))
+    if (verbose) print(paste('Convert with base', paste(base, collapse=', ')))
     if (is.character(base)){
       if (all(base %in% names(baselist))){
         base <- baselist[base]
@@ -44,23 +45,25 @@ length2SI <- function(x, units='imperial', base=NULL){
     if (ncol(x) == length(base)){
       x <- as.matrix(x)
       ## replace missing values with zero (but not for base metric)
-      xtmp <- x[,-1]
-      xtmp[is.na(xtmp)] <- 0
-      x[,2:ncol(x)] <- xtmp
+      if (ncol(x) > 1) {
+        xtmp <- x[,-1]
+        xtmp[is.na(xtmp)] <- 0
+        x[,2:ncol(x)] <- xtmp
+      }
       xout <- as.vector(x %*% base)
     } else {
       stop('Length of base for conversion does not match input')
     }
   } else {
     
-    if (units %in% names(baselist)){
+    if (!is.null(units) & units %in% names(baselist)){
       xout <- x * baselist[units]
     } else{
       stop('Units not in baselist')
     }
     names(xout) <- NULL
     
-    print(paste('Convert', units, 'to m'))
+    if (verbose) print(paste('Convert', units, 'to m'))
   }
 
   return(xout)
