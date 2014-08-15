fcd186c3fe6c18ebbd3ce2265c49219df26fff5c
Jonas Bhend <jonas.bhend@gmx.ch>
2014-08-15 13:41:23 +0200

    Fixed bug with computation of hour

--- R/read_Sweden.R
+++ R/read_Sweden.R
@@ -17,40 +17,33 @@ read_Sweden <- function(infile){
   ## reorganise data frame
   ## only melt pressure readings first
   rawmelt <- melt(rawdata[, c(1:3, grep('inHg', names(rawdata)))], 1:3, value.name='P')
-  rawmelt$hour <- toupper(gsub('.*\\.', '', rawmelt$variable))
+  ## extract observing hour
+  rawmelt$Time <- toupper(gsub('.*\\.', '', rawmelt$variable))
   rawmelt <- rawmelt[, -grep('variable', names(rawmelt))]
   ## also add in the temperature data
   if (length(grep('C', names(rawdata))) > 1){
     rawtmp <- melt(rawdata[, c(1:3, grep('X.C.', names(rawdata)))], 1:3, value.name='TP')
-    rawtmp$hour <- toupper(gsub('.*\\.', '', rawtmp$variable))
+    ## extract observing hour
+    rawtmp$Time <- toupper(gsub('.*\\.', '', rawtmp$variable))
     rawtmp <- rawtmp[, -grep('variable', names(rawtmp))]
     ## merge the two data frames 
     rawmelt <- merge(rawmelt, rawtmp)    
   }
   
-  ## extract observing hours
-  hour <- toupper(gsub('\\.', '', gsub('inHg.', '', rawmelt$variable)))
-
   ## correct hours if observing times differ throughout the year
   print(names(rawdata)[ncol(rawdata)])
   if ('X.from.May.to.October..6am.2pm.10pm' %in% names(rawdata)){
     ## set early hour to 6AM
-    hour[rawmelt$Month %in% 5:10 & hour == '7AM'] <- '6AM'
-    hour[rawmelt$Month %in% 5:10 & hour == '9PM'] <- '10PM'
+    rawmelt$Time[rawmelt$Month %in% 5:10 & rawmelt$Time == '7AM'] <- '6AM'
+    rawmelt$Time[rawmelt$Month %in% 5:10 & rawmelt$Time == '9PM'] <- '10PM'
   } else if ('X.6am.from.March.to.October' %in% names(rawdata)){
-    hour[rawmelt$Month %in% 3:10 & hour == '7AM'] <- '6AM'
+    rawmelt$Time[rawmelt$Month %in% 3:10 & rawmelt$Time == '7AM'] <- '6AM'
   }
 
-  ## add in time
-  rawmelt$Time <- hour
-  
   ## convert units of pressure reading
   rawmelt$Tcorr <- 0
   rawmelt$TP.units <- 'C'
-  
-  ## remove the variable names
-  rawmelt <- rawmelt[,-grep('variable', names(rawmelt))]
-  
+    
   ## add in the station name
   rawmelt$Station <- gsub('.*\\/', '', gsub('\\..*', '', infile))
   rawmelt$P.units <- 'Swedish inches (dec tum)'
